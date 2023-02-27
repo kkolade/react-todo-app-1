8ab7904ed2df07d336a101e4bc219eaeb18df1b6
Kola Kolade <kolakolade@gmail.com>
2023-02-27 19:38:52 +0100

    updated the TodoLogic to display hardcoded todos

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,4 +1,27 @@
 const TodosLogic = () => {
-  return <div>TodosLogic content</div>;
+  const todos = [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ];
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  );
 };
 export default TodosLogic;
